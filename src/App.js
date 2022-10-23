e7916b9ced87f1368fa61b4367e82aafc18dc750
Adeyemo Fortunatus <adeyemolekan433@gmail.com>
2022-10-23 01:24:39 +0100

    Stage one done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './components/Footer'
 import Main from './components/Main'
 import Social from './components/Social'
 
@@ -8,6 +9,7 @@ function App() {
        <div className='flex flex-col items-center py-[64px] px-[0px] gap-[24px]'>
           <Main />
           <Social />
+          <Footer />
        </div>
     </div>
   )
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,8 +6,8 @@ function Social() {
        <div className='flex flex-col items-center py-[0px] px-[16px] gap-[32px] w-[375px] h-[24px]'>
          <div className='flex flex-row items-center p-0 w-[343px] h-[24px]'>
             <div className='flex flex-row items-center justify-center p-0 gap-[24px] w-[343px] h-[24px]'>
-               <img id="slack" className='w-[24px] h-[24px]' src="./images/slack.png" alt="slack" />
-               <img id="slack" width="24px" height="24px" src="./images/Social icon.png" alt="slack" />
+              <a href="Natus05.slack.com" target="_blanck"> <img id="slack" className='w-[24px] h-[24px] mr-[10px]' src="./images/slack.png" alt="slack" /> </a>
+              <a href="https://github.com/Natus05" target="_blanck"> <img id="slack" width="24px" height="24px" src="./images/Social icon.png" alt="social" /> </a>
             </div>
          </div>
        </div>
