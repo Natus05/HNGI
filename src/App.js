0ede42349738178c79ebb3559037b9b8fc203254
Adeyemo Fortunatus <adeyemolekan433@gmail.com>
2022-11-05 16:08:37 +0100

    back to router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import Contact from './Contact'
 import Home from './Home'
-import { BrowserRouter as Routes, Route } from 'react-router-dom'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 
 
 function App() {
   return (
     <div className='bg-[#ffffff]'>
-      <HashRouter>
+      <Router>
         <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/contact' element={<Contact/>} />
        </Routes>
        <Footer />
-      </HashRouter>
+      </Router>
     </div>
   )
 }
